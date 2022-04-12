2a570380d0b71bab95264db5304574a2b7ed54ee
RoflPolarity <ignis604@gmail.com>
2022-04-12 23:32:18 +0300

    null

--- src/main/java/lab3.java
+++ src/main/java/lab3.java
@@ -1,3 +1,10 @@
+import javax.swing.*;
 public class lab3 {
 
-}
+    public static void main(String[] args) {
+        Visualixations vis = new Visualixations();
+    }
+    static class Visualixations extends JFrame{
+
+        }
+    }
