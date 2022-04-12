9bb7d4775541220ffdadb56244ca61e5b4b8d5bf
RoflPolarity <ignis604@gmail.com>
2022-04-13 02:09:48 +0300

    vorkedGraphViz

--- src/main/java/lab3.java
+++ src/main/java/lab3.java
@@ -1,10 +1,30 @@
-import javax.swing.*;
+import guru.nidi.graphviz.attribute.Color;
+import guru.nidi.graphviz.attribute.Font;
+import guru.nidi.graphviz.attribute.Rank;
+import guru.nidi.graphviz.attribute.Style;
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.Graph;
+import java.io.File;
+import java.io.IOException;
+import static guru.nidi.graphviz.attribute.Attributes.attr;
+import static guru.nidi.graphviz.attribute.Rank.RankDir.LEFT_TO_RIGHT;
+import static guru.nidi.graphviz.model.Factory.graph;
+import static guru.nidi.graphviz.model.Factory.node;
+import static guru.nidi.graphviz.model.Link.to;
+
 public class lab3 {
-
-    public static void main(String[] args) {
-        Visualixations vis = new Visualixations();
+    public static void main(String[] args) throws IOException {
+        Graph g = graph("example1").directed()
+                .graphAttr().with(Rank.dir(LEFT_TO_RIGHT))
+                .nodeAttr().with(Font.name("arial"))
+                .linkAttr().with("class", "link-class")
+                .with(
+                        node("a").with(Color.RED).link(node("b")),
+                        node("b").link(
+                                to(node("c")).with(attr("weight", 5), Style.DASHED)
+                        )
+                );
+        Graphviz.fromGraph(g).height(100).render(Format.PNG).toFile(new File("example/ex1.png"));
     }
-    static class Visualixations extends JFrame{
-
-        }
-    }
+}
