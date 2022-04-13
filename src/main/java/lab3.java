ebd02b1c5a952e5d60805ccdd226d3cd319271e0
RoflPolarity <43727574+RoflPolarity@users.noreply.github.com>
2022-04-13 04:29:35 +0300

    ~3 lab

--- src/main/java/lab3.java
+++ src/main/java/lab3.java
@@ -1,30 +1,42 @@
-import guru.nidi.graphviz.attribute.Color;
-import guru.nidi.graphviz.attribute.Font;
-import guru.nidi.graphviz.attribute.Rank;
-import guru.nidi.graphviz.attribute.Style;
+import guru.nidi.graphviz.attribute.*;
 import guru.nidi.graphviz.engine.Format;
 import guru.nidi.graphviz.engine.Graphviz;
-import guru.nidi.graphviz.model.Graph;
+import guru.nidi.graphviz.model.*;
+
+import javax.lang.model.element.Name;
 import java.io.File;
 import java.io.IOException;
 import static guru.nidi.graphviz.attribute.Attributes.attr;
 import static guru.nidi.graphviz.attribute.Rank.RankDir.LEFT_TO_RIGHT;
-import static guru.nidi.graphviz.model.Factory.graph;
-import static guru.nidi.graphviz.model.Factory.node;
+import static guru.nidi.graphviz.model.Factory.*;
 import static guru.nidi.graphviz.model.Link.to;
 
 public class lab3 {
     public static void main(String[] args) throws IOException {
-        Graph g = graph("example1").directed()
-                .graphAttr().with(Rank.dir(LEFT_TO_RIGHT))
-                .nodeAttr().with(Font.name("arial"))
-                .linkAttr().with("class", "link-class")
-                .with(
-                        node("a").with(Color.RED).link(node("b")),
-                        node("b").link(
-                                to(node("c")).with(attr("weight", 5), Style.DASHED)
-                        )
-                );
-        Graphviz.fromGraph(g).height(100).render(Format.PNG).toFile(new File("example/ex1.png"));
+        String[] alf = {"S0","S1","S2"}, count = {"a","b"};
+        String[][][] in_count= {{{"S0","S1","S2"},{"S0","S2"}},
+                                {{"S0","S1","S2"},{"S0","S2"}},
+                                {{},{"S0","S2"}}};
+        visual_graph(alf,count,in_count,null);
+    }
+
+    public static void visual_graph(String[]alfist,String[]countlist,String[][][]in_countlist,String filename) throws IOException {
+        MutableGraph dot = mutGraph("g1").setDirected(true);
+        for (int i = 0; i < alfist.length; i++) {
+            dot.add(mutNode(alfist[i]));
+        }
+        for (int i = 0; i < alfist.length; i++) {
+            for (int j = 0; j < in_countlist[i].length; j++) {
+                String[] val = in_countlist[i][j];
+                if (val.length!=0){
+                    for (String sym:val) {
+                        dot.add(mutNode(alfist[i])
+                                .addLink(mutNode(sym).
+                                        add(Label.of(countlist[j]))));
+                    }
+                }
+            }
+        }
+        Graphviz.fromGraph(dot).height(600).width(800).render(Format.PNG).toFile(new File("example/ex1.png"));
     }
 }
